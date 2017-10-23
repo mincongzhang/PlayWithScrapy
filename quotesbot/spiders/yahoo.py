812d8d4fc62130eddca986e45814219e414bd941
Mincong <mincong.zhang@hotmail.com>
2017-10-23 14:50:17 +0100

    Update yahoo.py

--- quotesbot/spiders/yahoo.py
+++ quotesbot/spiders/yahoo.py
@@ -7,12 +7,12 @@ class ToScrapeSpiderYahoo(scrapy.Spider):
     name = 'yahoo'
     
     def start_requests(self):
-    urls = [
-        'https://uk.finance.yahoo.com/quote/AAPL/holders?p=AAPL',
-        'https://uk.finance.yahoo.com/quote/BABA/holders?p=BABA',
-    ]
-    for url in urls:
-        yield scrapy.Request(url=url, callback=self.parse)
+        urls = [
+            'https://uk.finance.yahoo.com/quote/AAPL/holders?p=AAPL',
+            'https://uk.finance.yahoo.com/quote/BABA/holders?p=BABA',
+        ]
+        for url in urls:
+           yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         rows = response.xpath("//tr")
