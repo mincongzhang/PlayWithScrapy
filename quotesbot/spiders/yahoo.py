adada96d68e52e760fa04d5f37320a6353934049
Mincong <mincong.zhang@hotmail.com>
2017-10-23 14:48:37 +0100

    Update yahoo.py

--- quotesbot/spiders/yahoo.py
+++ quotesbot/spiders/yahoo.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+#https://stackoverflow.com/questions/41472582/first-time-using-scrapy-trying-to-crawl-a-set-of-tables
 
 class ToScrapeSpiderYahoo(scrapy.Spider):
     name = 'yahoo'
+    
+    def start_requests(self):
     start_urls = [
         'https://uk.finance.yahoo.com/quote/AAPL/holders?p=AAPL',
         'https://uk.finance.yahoo.com/quote/BABA/holders?p=BABA',
