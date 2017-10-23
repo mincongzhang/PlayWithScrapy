cd1511f699a3282c2fdc2584427249453ac0269a
Mincong <mincong.zhang@hotmail.com>
2017-10-23 14:43:03 +0100

    Update yahoo.py

--- quotesbot/spiders/yahoo.py
+++ quotesbot/spiders/yahoo.py
@@ -1,1 +1,20 @@
+# -*- coding: utf-8 -*-
+import scrapy
 
+
+class ToScrapeSpiderXPath(scrapy.Spider):
+    name = 'toscrape-xpath'
+    start_urls = [
+        'https://uk.finance.yahoo.com/quote/AAPL/holders?p=AAPL',
+        'https://uk.finance.yahoo.com/quote/BABA/holders?p=BABA',
+    ]
+    for url in urls:
+        yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        rows = response.xpath("//tr")
+        for row in rows:
+            text = row.xpath(".//td/text()").extract_first()
+            yield {
+                'text': text
+            }
